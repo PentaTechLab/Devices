21ba9049d7d7f82dd9598e8744d9417b602723ee
Taras Petriv <taras.petriv@plvision.eu>
2016-03-02 23:28:36 +0200

    put into folders

---
+++ Svitlofor/Svitlofor.ino
@@ -0,0 +1,110 @@
+// ---------------------------------------------------------------------------
+#include <NewPing.h>
+
+#define SONAR_NUM     6 // Number or sensors.
+#define MAX_DISTANCE 600 // Maximum distance (in cm) to ping.
+#define PING_INTERVAL 277// Milliseconds between sensor pings (29ms is about the min to avoid cross-sensor echo).
+
+unsigned long pingTimer[SONAR_NUM]; // Holds the times when the next ping should happen for each sensor.
+unsigned int cm[SONAR_NUM];         // Where the ping distances are stored.
+uint8_t currentSensor = 0;          // Keeps track of which sensor is active.
+int count = 0;                      // Count of cars
+
+NewPing sonar[SONAR_NUM] = {     // Sensor object array.
+  NewPing(22, 23, MAX_DISTANCE), // Each sensor's trigger pin, echo pin, and max distance to ping.
+  NewPing(24, 25, MAX_DISTANCE),
+  NewPing(26, 27, MAX_DISTANCE),
+  NewPing(28, 29, MAX_DISTANCE),
+  NewPing(30, 31, MAX_DISTANCE),
+  NewPing(32, 33, MAX_DISTANCE)
+  
+};
+
+int led[SONAR_NUM] = {36,37,38,39,40,41};
+int rel[SONAR_NUM] = {2,3,4,5,6,7};
+
+int n =0;
+bool car[SONAR_NUM];
+void detect(int i)
+{ car[i]=true;
+  count=count+1; 
+  Serial.println("Place is occupied");
+  digitalWrite(led[i],LOW);
+  digitalWrite(rel[i],LOW); 
+
+}
+
+void free(int i)
+{ car[i]=false;
+  Serial.println("Place is free");  
+  digitalWrite(led[i],HIGH); 
+  digitalWrite(rel[i],HIGH); 
+}
+void setup() {
+  Serial.begin(9600);
+  Serial.println("START");
+  pingTimer[0] = millis() + 100;           // First ping starts at 75ms, gives time for the Arduino to chill before starting.
+  for (uint8_t i = 1; i < SONAR_NUM; i++) // Set the starting time for each sensor.
+    pingTimer[i] = pingTimer[i - 1] + PING_INTERVAL;
+    
+    for (n=0; n<SONAR_NUM; n++) 
+    {
+      pinMode(led[n], OUTPUT);
+      pinMode(rel[n], OUTPUT);
+      free(n);
+    }
+
+}
+
+void loop() {
+  for (uint8_t i = 0; i < SONAR_NUM; i++) { // Loop through all the sensors.
+    if (millis() >= pingTimer[i]) {         // Is it this sensor's time to ping?
+      pingTimer[i] += PING_INTERVAL * SONAR_NUM;  // Set next time this sensor will be pinged.
+      if (i == 0 && currentSensor == SONAR_NUM - 1) oneSensorCycle(); // Sensor ping cycle complete, do something with the results.
+      sonar[currentSensor].timer_stop();          // Make sure previous timer is canceled before starting a new ping (insurance).
+      currentSensor = i;                          // Sensor being accessed.
+      cm[currentSensor] = 0;                      // Make distance zero in case there's no ping echo for this sensor.
+      sonar[currentSensor].ping_timer(echoCheck); // Do the ping (processing continues, interrupt will call echoCheck to look for echo).
+    }
+  }
+  // The rest of your code would go here.
+}
+
+void echoCheck() { // If ping received, set the sensor distance to array.
+  if (sonar[currentSensor].check_timer())
+    cm[currentSensor] = sonar[currentSensor].ping_result / US_ROUNDTRIP_CM;
+}
+
+void oneSensorCycle() { // Sensor ping cycle complete, do something with the results.
+  for (uint8_t i = 0; i < SONAR_NUM; i++) {
+
+    
+    Serial.print(i);
+    Serial.print("=");
+    Serial.print(cm[i]); 
+    Serial.println("cm ");
+    
+    if (i == 0 || i == 5)
+    {
+    if (cm[i] > 33)
+    {
+     if (car[i] == false)
+     { if (cm[i]<=377) detect(i); } 
+     else 
+     { if (cm[i]>377) free(i);}
+     }
+     else if (cm[i] == 0) free(i); 
+    }
+    else 
+    {
+      if (cm[i] > 33)
+    {
+     if (car[i] == false)
+     { if (cm[i]<=333) detect(i); } 
+     else 
+     { if (cm[i]>333) free(i);}
+     }      
+    }
+  }
+  Serial.println();
+}
---
+++ Svitlofor/Svitlofor_classes.ino
@@ -0,0 +1,105 @@
+#include <NewPing.h>
+#define maximum 500
+
+// ========================================
+bool car = false;
+
+int trig1 = 22; 
+int trig2 = 24;
+int trig3 = 26; 
+int trig4 = 28;
+int trig5 = 30; 
+int trig6 = 32;
+
+int echo1 = 23;
+int echo2 = 25;
+int echo3 = 27;
+int echo4 = 29;
+int echo5 = 31;
+int echo6 = 33;
+
+
+// 
+int count = 0;
+int usec;
+int i;
+
+class Portal : public NewPing   
+{
+  private:
+   int num;
+   int green_led;
+   int red_led;
+   int trig;
+  public:
+  bool car = false;
+   Portal(int n, int trig, int pos, int g, int r):NewPing(trig, pos, maximum)
+   {
+    num = n; 
+    green_led = g; 
+    red_led  = r;
+    pinMode(green_led, OUTPUT);
+    pinMode(red_led, OUTPUT); 
+    this->detect();
+   }
+
+
+void locate ()
+ {
+ usec=this->ping_cm(); //sends ping and estimates the duration of echo in uS
+ Serial.print("Sonar" );
+ Serial.print(num);
+ Serial.print(" ");
+ Serial.println(usec);
+ 
+ if (usec <10 || usec==0) return;
+ else
+ {
+ if (car == true)
+ { if (usec>50) free(); } 
+ else 
+ { if (usec<=50) detect();}
+ }
+ }
+
+void detect()
+{ car=true;
+  count=count+1; 
+  Serial.println("Place is occupied");
+  Serial.print("Total cars :");
+  Serial.println(count);
+  digitalWrite(green_led,LOW);
+  digitalWrite(red_led,LOW);
+
+}
+
+void free()
+{ car=false;
+  Serial.println("Place is free");  
+  digitalWrite(red_led,HIGH);  
+  digitalWrite(green_led,HIGH);
+}
+
+};
+//
+  Portal P1(1,trig1,echo1,36,37);
+  Portal P2(2,trig2,echo2,38,39);
+  Portal P3(3,trig3,echo3,40,41);
+//  Portal P4(4,trig4,echo4,5,39);
+//  Portal P5(5,trig5,echo5,6,40);
+//  Portal P6(6,trig6,echo6,7,41);
+
+void setup()
+{ 
+  Serial.begin(9600);
+  Serial.println("START"); 
+}
+  
+void loop()
+{ 
+  P1.locate();
+  delay(500);
+
+  P2.locate();
+  delay(500);
+}
